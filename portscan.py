41e837dac6f18abbe9f0354446ab29b0fc4aeb88
Rodrigo Solaris <78048715+solariscodes@users.noreply.github.com>
2021-03-03 00:26:07 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -3,20 +3,30 @@
 # Rodrigo march 2, 2021
 #
 import socket
+import sys
 
-host='www.terra.com.br'
-ports=(441,444)
+if len(sys.argv) <= 3:
+    print("Wrong syntax!")
+    print("Syntax: portscan.py HOST PORT RANGE")
+    print("Example: portscan.py www.nasa.gov 1 80")
+    sys.exit(1)
+
+host=sys.argv[1]
+ports=(int(sys.argv[2]),int(sys.argv[3]))
 
 socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def solveDns(self):
-    self=socket.gethostbyname(self)
+    try:
+        self=socket.gethostbyname(self)
+    except:
+        print("Unable to resolve",host)
+        sys.exit(1)
     return self
 
 def checkPort(self):
     global solved
     solved=solveDns(host)
-    #solved='192.168.1.1'
     fulladress=(solved,self)
     isalive=socket1.connect_ex(fulladress)
     if isalive == 0:
@@ -27,7 +37,7 @@ def checkPort(self):
 def scan():
     global num
     num=0
-    print("Portscan TCP by solariscodes.\nScanning",host,"\n")
+    print("Portscan TCP by solariscodes\nScanning",host,"\nThis may take a while...")
     for i in range(ports[0],ports[-1]):
         a = checkPort(i)
         print("Port", i, "is", a)
